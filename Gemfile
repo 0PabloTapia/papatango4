9684739bf1b9c577896f25a1852b68f87d3c8715
pablo-tapia <pablotapiavasquez@gmail.com>
2019-10-29 16:20:49 -0300

    thingscommit

--- Gemfile
+++ Gemfile
@@ -1,10 +1,14 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+gem "chartkick"
 gem 'omniauth-facebook'
 ruby '2.6.3'
 gem 'devise'
 gem 'activeadmin'
 gem 'image_processing', '~> 1.2'
+group :production do
+  gem "aws-sdk-s3", require: false
+  end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.0'
 # Use postgresql as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,13 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'home/index', as: 'home'
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :users
+  devise_for :users, controllers: {
+    
+     omniauth_callbacks: "users/omniauth_callbacks", 
+        registrations: 'users/registrations'
+      }
+
+    resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
