86c08ec3850272595ac831de36f3719b93f03f3f
pablo-tapia <pablotapiavasquez@gmail.com>
2019-10-30 11:41:03 -0300

    new

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
+	
+	resources :profiles do
+		delete 'delete_image/:image_id', on: :member, to: 'profiles#delete_image',
+		as: 'delete_image'
+	end
+	resources :profiles
+	devise_for :admin_users, ActiveAdmin::Devise.config
+	ActiveAdmin.routes(self)
+	get 'home/index', as: 'home'
+	devise_for :users, controllers: {
+	
+	omniauth_callbacks: "users/omniauth_callbacks", 
+	registrations: 'users/registrations'
+}
 
-  resources :profiles do
-    delete 'delete_image/:image_id', on: :member, to: 'profiles#delete_image',
-   as: 'delete_image'
-  end
-  resources :profiles
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  get 'home/index', as: 'home'
-  devise_for :users, controllers: {
-    
-     omniauth_callbacks: "users/omniauth_callbacks", 
-        registrations: 'users/registrations'
-      }
-
-    resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+resources :users
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: "home#index"
 end
